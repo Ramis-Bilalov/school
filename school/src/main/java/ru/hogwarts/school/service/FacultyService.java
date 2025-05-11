bd8b96f596ab73f0f4dad56134193d5b75fd2092
RamisDeveloper <ramiska81@gmail.com>
2025-05-11 23:42:32 +0300

    Логирование и конфигурирование в приложении - сдача д/з

--- school/src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ school/src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,36 +13,45 @@ import java.util.Collection;
 public class FacultyService {
     private final FacultyRepository facultyRepository;
 
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
+        logger.info("Was invoked method for find faculty");
         return facultyRepository.findById(id).get();
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Was invoked method for edit faculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Was invoked method for delete faculty");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> getAllFaculties() {
+        logger.info("Was invoked method for get all faculties");
         return facultyRepository.findAll();
     }
 
     public Faculty getFacultyByName(String name) {
+        logger.info("Was invoked method for get faculty by name");
         return facultyRepository.findFacultyByNameIgnoreCase(name);
     }
 
     public Collection<Student> getFacultyStudents(String name) {
         Faculty faculty = facultyRepository.findFacultyByNameIgnoreCase(name);
+        logger.info("Was invoked method for get students of faculty");
         return faculty.getStudents();
     }
 }
